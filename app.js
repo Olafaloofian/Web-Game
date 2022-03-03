3671250f4d4c415bd059491b577f255c2fc0e626
Nghia Nguyen <aihgn224@gmail.com>
2022-03-02 20:06:24 -0800

    added function for scoreboard

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-const holes= document.querySelectorAll('.hole');
-const scoreBoard= document.querySelectorAll('.score');
-const zombie= document.querySelectorAll('.zombie');
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const zombies  = document.querySelectorAll('.zombie');
 let lastHole;
 let timeUp = false;
 let score = 0;
 
-//function make random time for zombie to pop up from hole
-function randomTime(min, max){
-    return Math.round(Math.random() * (max - min)+min);
+//create a function to make a random time for mole to pop from the hole
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes){
-    const index = Math.floor(Math.random() * holes.length);
-    const hole =holes[index];
+    const index  = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
 
-    if(hole === lastHole){
-        return randomHole(hole);
+    //prevent same hole from getting the same number
+    if (hole === lastHole){
+        return randomHole(holes);
     }
-    lastHole=hole;
+    lastHole = hole;
     return hole;
-
 }
 
-function peep(){
-    const time= randomTime(500,1000);
-    const hole= randomHole(holes);
-    hole.classList.add('up');
-    setTimeout(()=>{
-        hole.classList.remove('up');
-        if(!timeUp)
+function peep() {
+    const time = randomTime(500, 1000); //get a random time to determine how long mole should peep
+    const hole = randomHole(holes); //get the random hole from the randomHole function
+    hole.classList.add('up'); //add the CSS class so selected mole can "pop up"
+    setTimeout(() => {
+        hole.classList.remove('up'); //make the selected mole "pop down" after a random time
+        if(!timeUp) {
             peep();
-        
+        }
     }, time);
 }
 
-function startGame(){
+function startGame() {
     scoreBoard.textContent = 0;
-    timeUp =false;
+    timeUp = false;
     score = 0;
     peep();
-    setTimeout(() => timeUp = true, 10000)
+    setTimeout(() => timeUp = true, 15000) //show random moles for 15 seconds
 }
 
-
-document.getElementById('zombie').onclick=function(){
-    var score = parsInt(document.getElementById('score').innerHTML);
+function bonk(e){
+    if(!e.isTrusted) return; //** new thing I learned */
     score++;
-    document.getElementById('score').innerHTML = score;
+    this.parentNode.classList.remove('up'); //this refers to item clicked
+    scoreBoard.textContent = score;
 }
+
+zombies.forEach(zombie => zombie.addEventListener('click', bonk))
